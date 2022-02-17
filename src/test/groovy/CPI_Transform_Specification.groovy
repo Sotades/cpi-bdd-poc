e4e4227ef099dfb7084092bb9a8524ae4f2d54df
Sotades <anthonyjbateman@gmail.com>
2022-02-17 20:33:47 +0200

    First test case first field.

--- src/test/groovy/CPI_Transform_Specification.groovy
+++ src/test/groovy/CPI_Transform_Specification.groovy
@@ -6,12 +6,12 @@ import spock.lang.Unroll
 class CPI_Transform_Specification extends Specification {
 
     @Unroll
-    def "Testing CPI Transform"() {
+    def "Testing as-is CPI Transform"() {
 
         given: "I set up the FileHandler and HTTPConnectionHandler"
         FileHandler fileHandler = new FileHandler(pathAndFileName)
         def fileContents = fileHandler.readFileContents()
-        def urlAddress = 'https://cpit100.it-cpi005-rt.cfapps.eu20.hana.ondemand.com/http/http_test_test'
+        def urlAddress = 'https://cpit100.it-cpi005-rt.cfapps.eu20.hana.ondemand.com/http/Global_SalesOrder_to_S4_HTTP_Receiver/BDD_Test/as_is'
         def user = 'sb-18fab321-4609-47c4-9718-ae9b2bce3726!b3328|it-rt-cpit100!b259'
         def password = 'c9316225-46fb-4692-bf94-6f8f4c5a2494$83F8QIJp3JT5-sp-L_J-banOvlHugLdrcnBXNw5mRGI='
 
@@ -23,16 +23,13 @@ class CPI_Transform_Specification extends Specification {
         def responseXml = new XmlSlurper().parseText(response)
 
         then: "I expect the following fields to match the expected results"
-        responseXml.IDOC.E1EDK01.CURCY == curcy
-        responseXml.IDOC.E1EDK01.HWAER == hwaer
-        responseXml.IDOC.E1EDK01.NTGEW == ntgew
-        responseXml.IDOC.E1EDK01.GEWEI == gewei
+        responseXml.ZALMA_SALESORDERCREATEFROMD201.IDOC.Z1ZALMA_SALESORDERCREATEFRO.E1BPSDHD1.DOC_TYPE == doc_type
+
 
         where: "test files and expected results are"
-        pathAndFileName             || curcy | hwaer | ntgew    | gewei
-        'tests/INVOIC_CU Test1.xml' || 'SEK' | 'SEK' | 153.120  | 'KGM'
-        'tests/INVOIC_CU Test2.xml' || 'GBP' | 'GBP' | 221.937  | 'LTR'
-        'tests/INVOIC_CU Test3.xml' || 'USD' | 'USD' | 451.216  | 'EA'
+        pathAndFileName               || doc_type
+        'tests/SalesOrder_urakka.xml' || 'ZED'
+
 
     }
 }
